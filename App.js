c56376144f8f914bb6996b50853293b4ede37800
Eric Sun <ericsun19591827346@gmail.com>
2022-03-08 21:17:56 -0800

    Added facial keypoints detection

--- App.js
+++ App.js
@@ -8,25 +8,28 @@ import { Buffer } from 'buffer'
 
 // Must import & override fetch otherwise will get "FileReader.readAsArrayBuffer is not implemented."
 // Causes "Platform browser has already been set. Overwriting the platform with [object Object]."
-import '@tensorflow/tfjs-react-native'
+import { bundleResourceIO } from '@tensorflow/tfjs-react-native'
 import * as mobilenet from '@tensorflow-models/mobilenet'
-// import * as cocossd from '@tensorflow-models/coco-ssd'
+import * as cocossd from '@tensorflow-models/coco-ssd'
+
+import modelJson from './nn_model/tfjs_model/model.json'
+import modelWeights from './nn_model/tfjs_model/group1-shard1of1.bin'
 
 export default function App() {
 
   const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [type, setType] = useState(Camera.Constants.Type.front);
 	const [cameraReady, setCameraReady] = useState(false)
 	const [tfReady, setTfReady] = useState(false)
 	const camera = useRef(null)
 	const [photo, setPhoto] = useState(null)
 	const [sm, setSm] = useState(null)
 	const [mm, setMm] = useState(null)
+	const [cm, setCm] = useState(null)
 	const [sm_pred, setSmPred] = useState([])
 	const [mm_pred, setMmPred] = useState([])
-	const [narration, setNarration] = useState('')
-	const windowheight = Dimensions.get('window').height
-	const windowwidth = Dimensions.get('window').width
+	const [cm_pred, setCmPred] = useState(null)
+	const [across, setacross] = useState(96)
 
 	// Request permissions
   useEffect(() => {
@@ -37,55 +40,96 @@ export default function App() {
 		setPhoto(null)
   }, [])
 
+	useEffect(() => {
+		if (hasPermission) {
+			let dim = Dimensions.get('window')
+			setacross(dim.height > dim.width ? dim.width : dim.height)
+		}
+	}, [ hasPermission ])
+
 	// Load TensorFlow + models
 	useEffect( async () => {
 		// tf.setBackend('cpu').then(
-			tf.ready().then(async () => {
-				setTfReady(true)
-				mobilenet.load().then(setSm)
-				// cocossd.load().then(setMm)
-			})
+		tf.ready().then(async () => {
+			setTfReady(true)
+			mobilenet.load().then(setSm)
+			cocossd.load().then(setMm)
+			tf.loadLayersModel(
+				bundleResourceIO(
+					modelJson, 
+					modelWeights
+				)
+			).then(setCm)
+		})
 		// )
-		
 	}, [])
 
 	useEffect(async () => {
 		if (photo && tfReady) {
 
-			setNarration('Photo taken')
 			let image = jpeg.decode(
 				Buffer.from(
-					photo.base64, 
+					photo.base64,
+					// photo.base64.split('base64,')[1], 
 					'base64'
 				), 
 				{ useTArray: true, formatAsRGBA: false }
 			)
-
-			setNarration('Image decoded')
 
 			let tensor = tf.tensor3d(
 				image.data, 
 				[image.height, image.width, 3]
 			)
 
-			setNarration('Converted to tensor')
+			let greyscale = new Uint8Array(image.height * image.width)
 
-			if (sm) {
-				setSmPred(await sm.classify(tensor))
+			let i = 0
+			for (let i = 0; i < image.height * image.width; i++) {
+				greyscale[i] = 
+					image.data[3 * i + 0] * 0.2989 + 
+					image.data[3 * i + 1] * 0.5870 + 
+					image.data[3 * i + 2] * 0.1140
 			}
 
-			// if (mm) {
-			// 	mm.detect(tensor).then(setMmPred)
-			// }
+			let gs_tensor = tf.tensor4d(
+				greyscale,
+				[1, image.height, image.width, 1]
+			)
 
-			setNarration('Predicting...')
+			if (sm) {
+				sm.classify(tensor).then(setSmPred)
+			}
+
+			if (mm) {
+				mm.detect(tensor).then(predictions => {
+					setMmPred(predictions)
+
+					console.log('mm_prediction made')
+
+					console.log(predictions)
+					if (predictions.length && predictions[0].class === 'person') {
+						console.log('largest is person')
+						// ~80% of 96 + 96
+						if (predictions[0].bbox[2] + predictions[0].bbox[3] > 150) { 
+							console.log('mostly person')
+							if (cm) {
+								console.log('facial keypoints detection')
+								let predictions = cm.predict(gs_tensor)
+								predictions.data().then(setCmPred)
+								// cm.predict(gs_tensor).data().then(setCmPred)
+
+							}
+						}
+					}
+				})
+			}
 
 		}
 	}, [ photo ])
 
 	useEffect(() => {
-		console.log(sm_pred)
-	}, [sm_pred])
+		console.log(cm_pred)
+	}, [ cm_pred ])
 
 	// Deal with situations surrounding no / awaiting permissions
   if (hasPermission === null) {
@@ -101,92 +145,238 @@ export default function App() {
 	// GUI
   return (
     <>
-			{tfReady && sm ? 
+			{tfReady && sm && mm && cm ? 
 				<View style={styles.container}>
+					<View style={{
+						height: 100,
+						alignItems: 'center',
+						justifyContent: 'center',
+					}}>
+						<Text style={{color: 'black'}}>title</Text>
+					</View>
+					
 					{photo ? 
-						<Pressable
-							onPressOut={() => {
-								setPhoto(null)
-								setNarration('')
-								setSmPred([])
-								setMmPred([])
-							}}
-						>
-
-							<ImageBackground
-								style={{ width: windowwidth, height: windowheight}}
-								source={photo}
+						<>
+							<Pressable
+								onPressOut={() => {
+									if (sm_pred.length) {
+										setPhoto(null)
+										setSmPred([])
+										setMmPred([])
+										setCmPred(null)
+									}
+								}}
 							>
-								<View style={styles.middletext}>
-									{sm_pred.length ? 
-									sm_pred.map(e => <Text 
-										style={{ color: 'white'}}
-										key={e.className}
-									>{e.className + ' ' + e.probability}</Text>)
-									: <Text style={{ color: 'white'}}>{narration}</Text>}
-								</View>
-							</ImageBackground>
-						</Pressable>
+
+								<ImageBackground
+									style={{ 
+										width: across, 
+										height: across,
+									}}
+									source={photo}
+								>
+									{
+										mm_pred.map(e => <Text
+												key={e.class + ' ' + e.score}
+												style={[ styles.box, { 
+													marginLeft: e.bbox[0] / 96 * across,
+													marginTop: e.bbox[1] / 96 * across,
+													width: e.bbox[2] / 96 * across,
+													height: e.bbox[3] / 96 * across,
+												}]}
+											>
+												{e.class + ' ' + e.score}
+											</Text>
+										)
+									}
+									{ cm_pred ? 
+										<>
+											<View
+												style={[styles.rectangle, {
+													marginLeft: cm_pred[0] / 96 * across,
+													marginTop: cm_pred[1] / 96 * across,
+												}]}
+											/>
+											<View
+												style={[styles.rectangle, {
+													marginLeft: cm_pred[2] / 96 * across,
+													marginTop: cm_pred[3] / 96 * across,
+												}]}
+											/>
+											<View
+												style={[styles.rectangle, {
+													marginLeft: cm_pred[4] / 96 * across,
+													marginTop: cm_pred[5] / 96 * across,
+												}]}
+											/>
+											<View
+												style={[styles.rectangle, {
+													marginLeft: cm_pred[6] / 96 * across,
+													marginTop: cm_pred[7] / 96 * across,
+												}]}
+											/>
+											<View
+												style={[styles.rectangle, {
+													marginLeft: cm_pred[8] / 96 * across,
+													marginTop: cm_pred[9] / 96 * across,
+												}]}
+											/>
+											<View
+												style={[styles.rectangle, {
+													marginLeft: cm_pred[10] / 96 * across,
+													marginTop: cm_pred[11] / 96 * across,
+												}]}
+											/>
+											<View
+												style={[styles.rectangle, {
+													marginLeft: cm_pred[12] / 96 * across,
+													marginTop: cm_pred[13] / 96 * across,
+												}]}
+											/>
+											<View
+												style={[styles.rectangle, {
+													marginLeft: cm_pred[14] / 96 * across,
+													marginTop: cm_pred[15] / 96 * across,
+												}]}
+											/>
+											<View
+												style={[styles.rectangle, {
+													marginLeft: cm_pred[16] / 96 * across,
+													marginTop: cm_pred[17] / 96 * across,
+												}]}
+											/>
+											<View
+												style={[styles.rectangle, {
+													marginLeft: cm_pred[18] / 96 * across,
+													marginTop: cm_pred[19] / 96 * across,
+												}]}
+											/>
+											<View
+												style={[styles.rectangle, {
+													marginLeft: cm_pred[20] / 96 * across,
+													marginTop: cm_pred[21] / 96 * across,
+												}]}
+											/>
+											<View
+												style={[styles.rectangle, {
+													marginLeft: cm_pred[22] / 96 * across,
+													marginTop: cm_pred[23] / 96 * across,
+												}]}
+											/>
+											<View
+												style={[styles.rectangle, {
+													marginLeft: cm_pred[24] / 96 * across,
+													marginTop: cm_pred[25] / 96 * across,
+												}]}
+											/>
+											<View
+												style={[styles.rectangle, {
+													marginLeft: cm_pred[26] / 96 * across,
+													marginTop: cm_pred[27] / 96 * across,
+												}]}
+											/>
+											<View
+												style={[styles.rectangle, {
+													marginLeft: cm_pred[28] / 96 * across,
+													marginTop: cm_pred[29] / 96 * across,
+												}]}
+											/>
+										</>
+									:
+										<></>
+									}
+								</ImageBackground>
+							</Pressable>
+							<View style={styles.container}>
+								{
+									sm_pred.map(e => 
+										<Text 
+											style={{ 
+												color: 'black',
+												flex: 1,
+											}}
+											key={e.className}
+										>
+											{e.className + ' ' + e.probability}
+										</Text>
+									)
+								}
+							</View>
+						</>
 						:
 						<>
 							<Camera 
-								style={styles.camera} 
+								style={{ 
+									width: across, 
+									height: across,
+								}}
 								type={type}
 								onCameraReady={() => setCameraReady(true)}
 								ref={camera}
-							>
-								<View style={styles.buttonContainer}>
-									<Pressable
-										style={styles.button}
-										onPressOut={() => {
-											setType(
-												type === Camera.Constants.Type.back
-													? Camera.Constants.Type.front
-													: Camera.Constants.Type.back
-											);
-										}}
-									>
-										<Text style={styles.text}> Flip </Text>
-									</Pressable>
+							/>
 
-									<Pressable
-										style={styles.button}
-										onPressOut={async () => {
-											if (cameraReady) {
-												camera.current.takePictureAsync(
-													{quality: 1}
-												).then(p => {
-													if (type === Camera.Constants.Type.front) {
-														manipulateAsync(
-															p.uri, 
-															[
-																{ rotate: 180 }, 
-																{ flip: FlipType.Vertical }, 
-																{ crop: { 
-																	originX: 0, 
-																	originY: 0, 
-																	width: p.width - 1, 
-																	height: p.height - 1}
-																}, 
-																{ resize: { width: windowwidth } }
-															],
-															{ base64: true, compress: 1, format: SaveFormat.JPEG }
-														).then(setPhoto)
-													} else {
-														manipulateAsync(
-															p.uri, 
-															[{ resize: { width: windowwidth } }],
-															{ base64: true, compress: 1, format: SaveFormat.JPEG }
-														).then(setPhoto)
-													}
-												})
-											}
-										}}
-									>
-										<Text style={styles.text}> Clip </Text>
-									</Pressable>
-								</View>
-							</Camera>
+							<View style={styles.buttonContainer}>
+								<Pressable
+									style={styles.button}
+									onPressOut={() => {
+										setType(
+											type === Camera.Constants.Type.back
+												? Camera.Constants.Type.front
+												: Camera.Constants.Type.back
+										);
+									}}
+								>
+									<Text style={styles.text}> Flip </Text>
+								</Pressable>
+
+								<Pressable
+									style={styles.button}
+									onPressOut={async () => {
+										if (cameraReady) {
+											camera.current.takePictureAsync(
+												{quality: 0}
+											).then(p => {
+												let s = Math.min(p.width, p.height)
+												if (type === Camera.Constants.Type.front) {
+													manipulateAsync(
+														p.uri, 
+														[
+															{ rotate: 180 }, 
+															{ flip: FlipType.Vertical }, 
+															{ crop: { 
+																	originX: (p.width - s) / 2, 
+																	originY: (p.height - s) / 2, 
+																	width: s, 
+																	height: s
+																}
+															}, 
+															{ resize: { width: 96, height: 96 } }
+														],
+														{ base64: true, compress: 1, format: SaveFormat.JPEG }
+													).then(setPhoto)
+												} else {
+													manipulateAsync(
+														p.uri, 
+														[
+															{ crop: { 
+																	originX: (p.width - s) / 2, 
+																	originY: (p.height - s) / 2, 
+																	width: s, 
+																	height: s
+																}
+															}, 
+															{ resize: { width: 96, height: 96 } }
+														],
+														{ base64: true, compress: 1, format: SaveFormat.JPEG }
+													).then(setPhoto)
+												}
+											})
+										}
+									}}
+								>
+									<Text style={styles.text}> Clip </Text>
+								</Pressable>
+							</View>
 						</>
 					}
 				</View>
@@ -206,9 +396,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  camera: {
-    flex: 1,
-  },
   buttonContainer: {
     flex: 1,
     backgroundColor: 'transparent',
@@ -222,12 +409,24 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 18,
-    color: 'white',
+    color: 'black',
   },
 	middletext: {
 		flex: 1,
-		// backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
+	},
+	rectangle: {
+		height: 5,
+		width: 5,
+		backgroundColor: 'red',
+		position: 'absolute',
+		zIndex: 99,
+	},
+	box: {
+		position: 'absolute',
+		borderWidth: 2,
+		borderColor: 'red',
+		color: 'red',
 	}
-});
+})
---
+++ README.md
@@ -0,0 +1,32 @@
+# Facial Keypoints Detection
+
+## Purpose of project
+
+- what is this
+- how does it work
+	- data sources
+	- techniques
+	- test results
+- who is it by
+- goal
+
+## Installation
+
+- Python
+	1. Have python & pip installed
+	2. "pip install -r requirements.txt"
+	3. open ./nn_model/generate.ipynb with method of choice (e.g. VSCode + Jupyter extension)
+
+- React Native
+	1. Have npm installed
+	2. "npm install"
+	3. "expo start"
+	4. Open on web or Expo Go (Android & iOS)
+
+## Licensing
+
+tbd
+
+## Contact Info
+
+tbd
